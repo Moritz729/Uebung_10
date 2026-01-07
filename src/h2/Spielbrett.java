10a2594e1eaaad1a13f354a8950a3eaafafa9e7d
Moritz729 <moritz.abecker@gmx.de>
2026-01-07 08:47:30 +0100

    Add Spielbrett class with getters and setters

--- src/h2/Spielbrett.java
+++ src/h2/Spielbrett.java
@@ -1,5 +1,23 @@
 package h2;
 
 public class Spielbrett {
+   private Feld[][] Brett;
+    private int dim;
+    public Feld[][] getBrett() {
+        return Brett;
+    }
+    public void setBrett(Feld[][] brett) {
+        Brett = brett;
+    }
+    public int getDim() {
+        return dim;
+    }
+    public void setDim(int dim) {
+        this.dim = dim;
+    }
+    public Spielbrett(int dim){
+        this.dim=dim;
+        Brett=new Feld[dim][dim];
+    }
 
 }
